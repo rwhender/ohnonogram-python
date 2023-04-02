a4114291fac164d0f3afac15714b7b7514ba97b6
Wesley Henderson <wesley.henderson11@gmail.com>
2023-04-02 17:02:24 -0500

    Define initial solver interface

--- ohnonogram/solver.py
+++ ohnonogram/solver.py
@@ -1,3 +1,29 @@
 """
 Defines solver interface and solver implementations.
 """
+from abc import ABC, abstractmethod
+from typing import List
+
+from ohnonogram.nonogram import Nonogram, NonogramPuzzle
+
+
+class NonogramSolver(ABC):
+    """
+    Class defining the interface for nonogram solvers.
+    """
+
+    puzzle: NonogramPuzzle
+    """
+    Puzzle to be solved.
+    """
+    attempted_states: List[Nonogram]
+    """
+    A list of puzzle states attempted during solving for later analysis.
+    """
+
+    def __init__(self, puzzle: NonogramPuzzle):
+        self.puzzle = puzzle
+
+    @abstractmethod
+    def solve(self) -> Nonogram:
+        ...
